e30a239e4b473ee4f65c44430fcf3f9b99560782
damiantarnawsky <damiantarnawsky@gmail.com>
2025-02-03 20:55:04 -0800

    feat: Capacitor 7 Support

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { CapacitorDeviceCheckPlugin } from './definitions';
 
-const DeviceCheck = registerPlugin<CapacitorDeviceCheckPlugin>(
-  'CapacitorDeviceCheck',
-  {
-    web: () => import('./web').then(m => new m.CapacitorDeviceCheckWeb()),
-  },
-);
+const DeviceCheck = registerPlugin<CapacitorDeviceCheckPlugin>('CapacitorDeviceCheck', {
+  web: () => import('./web').then((m) => new m.CapacitorDeviceCheckWeb()),
+});
 
 export * from './definitions';
 export { DeviceCheck };
